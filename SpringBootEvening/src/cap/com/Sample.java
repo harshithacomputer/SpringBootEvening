39af401ad46f32bbb9738fa9b9820cd696ebcd3f
Dell <Dell@DESKTOP-9ESPFT0>
2025-08-29 14:24:49 +0100

    developer1 done

--- SpringBootEvening/src/cap/com/Sample.java
+++ SpringBootEvening/src/cap/com/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("This lead work...");
 		System.out.println("Lead Done..");
+		System.out.println("developer1 done");
 		
 
 	}
